beee56b86d1472ac425329aeeabd6f230b9513a9
Faiz-Muazzam <65695747+E31190648-Faiz-Muazzam@users.noreply.github.com>
2021-03-11 21:53:45 +0700

    Update tugas1.php

--- Minggu_1/Tugas/tugas1.php
+++ Minggu_1/Tugas/tugas1.php
@@ -9,4 +9,17 @@
 		public $tahunterbit;
 		public $cetakan;
 	}
+	//Membuat instance
+	$book = new Buku();
+	echo "<h1>Data Buku</h1>";
+	//Set values and get values
+	echo "Judul : ".$book -> judulbuku="Laskar Pelangi";
+	echo "<br>";
+	echo "Pengarang : ".$book -> pengarang="Tidak Tahu";
+	echo "<br>";
+	echo "Penerbit : ".$book -> penerbit="Tidak Tahu";
+	echo "<br>";
+	echo "Tahun Terbit : ".$book -> tahunterbit="2012";
+	echo "<br>";
+	echo "Cetakan : ".$book -> cetakan="Jakarta";
 ?>
