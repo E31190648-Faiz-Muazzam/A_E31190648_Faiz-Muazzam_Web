b697b638d4b7634e1edffde900715db4359f14e1
Faiz-Muazzam <65695747+E31190648-Faiz-Muazzam@users.noreply.github.com>
2021-03-12 13:12:49 +0700

    Create praktikum2_minggu3.php

---
+++ Minggu_3/Praktikum/praktikum2_minggu3.php
@@ -0,0 +1,23 @@
+<?php
+
+class Car {
+
+	//private
+	private $model;
+	// agar dapat menampilkan nilai dari property private maka nilai harus di deklarasikan terlebih dulu, contoh
+	// private $model="Marcedes Benz"; 
+
+	function getModel()
+	{
+		return "The car model is " . $this -> model;
+	}
+}
+
+$mercedes = new Car();
+
+////akses property dari luar class.
+// maka akan terjadi fatal eror
+$mercedes -> model = "Mercedes benz";
+echo $mercedes -> getModel();
+
+?>
