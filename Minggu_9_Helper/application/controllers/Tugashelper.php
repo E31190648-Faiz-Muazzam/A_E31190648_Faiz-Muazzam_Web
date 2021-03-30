9904002467cb04e9f08846357d1b7ab35241670d
Faiz-Muazzam <65695747+E31190648-Faiz-Muazzam@users.noreply.github.com>
2021-03-30 13:49:37 +0700

    E31190648_Faiz Muazzam_A

---
+++ Minggu_9_Helper/application/controllers/Tugashelper.php
@@ -0,0 +1,20 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Author Faiz Muazzam
+ */
+class Tugashelper extends CI_Controller
+{
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url', 'security', 'html', 'file');
+	}
+
+	public function index()
+	{
+		$this->load->view('Tugas/index');
+	}
+}
+ ?>
